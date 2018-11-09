582ab03a9ea75470ff075105731864bc1ce1ae46
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-09 20:57:40 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,18 +2,21 @@
 require "pry"
 
 class Backer
-  attr_accessor :name, :project, :backed_projects, :backers
+  attr_accessor :name, :project, :backers
+  attr_reader :backed_projects
   
   def initialize(name)
     @name = name
     @backed_projects = []
   end 
-
+  
+  def backed_projects=(project)
+      
+  end
+  
   def back_project(project)
-    if !project.backers.include?(self)
-      self.backed_projects << project
-      project.add_backer(self)
-    end
+    self.backed_projects << project
+    project.add_backer(self)
   end 
 
 end
