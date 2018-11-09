434ffd0ae118538ec7a86bf5c29868fa77690ab0
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-09 20:31:59 +0000

    automatically backed up by learn

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+
+require "pry"
+
+class Backer
+  attr_accessor :name, :project, :backed_projects, :backers
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end 
+
+  def back_project(project)
+    @backed_projects << project
+    Project.ad
+  end 
+
+end
